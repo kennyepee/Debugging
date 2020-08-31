016059c961642597d3e898df8416866f71b60a9b
Kenny Epee <kennyepee@Kennys-MacBook-Pro.local>
2020-08-31 17:56:21 -0400

    Initial commit

---
+++ src/com/company/ClassName.java
@@ -0,0 +1,13 @@
+package com.company;
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+import java.util.Scanner;
+public class ClassName {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("How was your day?");
+        String n = input.nextLine();
+    }
+}
